8476f752b6e08b97b27d77e68f5a250f69f0f38b
Rodrigo Caceres <rcaceres@framebox.cl>
2022-11-03 22:06:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req,res) => res.send('<h1>Recordatorio con express</h1>'))
+
+app.listen(3000);
+console.log('sirviendo en puerto 3000');
+
+
+/*
+const express = require('express');
+
+const app = express();
+
 app.get('/',(req,res) => res.send('Recordatorio con express'))
 
 app.listen(3000);
 console.log('sirviendo en puerto 3000');
+*/
